1caca276072d6413c5f949b4c234cc5e5b3e916b
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2024-04-15 00:26:34 +0530

    Add borderradius and blob animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      borderRadius: {
+        blob: "42% 56% 72% 28% / 42% 42% 56% 48%",
+      },
+      keyframes: {
+        morph: {
+          "0% 100%": {
+            borderRadius: "42% 56% 72% 28% / 42% 42% 56% 48%",
+          },
+          "33%": {
+            borderRadius: "72% 28% 48% 48% / 28% 28% 72% 72%",
+          },
+          "66%": {
+            borderRadius: "100% 56% 56% 100% / 100% 100% 56% 56%",
+          },
+        },
+      },
+      animation: {
+        morph: "morph 3.75s linear infinite",
+      },
     },
   },
   plugins: [],
