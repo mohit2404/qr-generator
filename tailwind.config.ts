286b15d86c0e00f74a29edc7986539291cd542f0
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2024-10-15 14:54:27 +0530

    add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,29 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      borderRadius: {
-        blob: "42% 56% 72% 28% / 42% 42% 56% 48%",
-      },
-      keyframes: {
-        morph: {
-          "0% 100%": {
-            borderRadius: "42% 56% 72% 28% / 42% 42% 56% 48%",
-          },
-          "33%": {
-            borderRadius: "72% 28% 48% 48% / 28% 28% 72% 72%",
-          },
-          "66%": {
-            borderRadius: "100% 56% 56% 100% / 100% 100% 56% 56%",
-          },
-        },
-      },
-      animation: {
-        morph: "morph 3.75s linear infinite",
+      colors: {
+        light: "#F8F8FF",
+        dark: "#0C0C0C",
       },
     },
   },
